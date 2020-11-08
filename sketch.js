b0e51c9b4847ae381617fc1dddf2782e6b250fb7
Yashvi Goyal <72339157+ygoyal1204@users.noreply.github.com>
2020-11-08 22:25:47 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ var backgroundImg,platform;
 var bird, slingShot;
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    backgroundImg = loadImage("bg.png");
 }
 
 function setup(){
